0e07b33042a5c2886cd37be63b946631dcf3a465
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-09-19 11:11:42 +0200

    hull wip

--- hull.lua
+++ hull.lua
@@ -1,80 +1,137 @@
 local MP = minetest.get_modpath(minetest.get_current_modname())
 
-local hull_full_rule = { groups = {"hull_full"} }
-local hull_slope_rule = { groups = {"hull_slope"} }
+local g_hull = {groups={"hull"}}
+local g_inside = {groups={"hull_inside"}}
 
 mapblock_tileset.register_tileset("hull", {
     catalog = MP .. "/schematics/hull.zip",
     groups = {
-        hull = true,
-        hull_full = true
+        hull = true
     },
     disable_orientation = {
         ["scifi_nodes:blackdmg"] = true
     },
     tiles = {
         {
-            -- all sides
+            -- center corner
+            positions = {{x=0,y=1,z=0}},
+            rules = {
+                ["0,0,1"] = g_inside,
+                ["-1,0,0"] = g_inside,
+                ["0,0,-1"] = g_hull,
+                ["1,0,0"] = g_hull,
+                ["0,-1,0"] = g_hull,
+                ["0,1,0"] = g_hull
+            },
+            rotations = {0,90,180,270}
+        },{
+            -- center straight
             positions = {{x=1,y=1,z=0}},
-            rules = {},
+            rules = {
+                ["0,0,1"] = g_inside
+            },
+            rotations = {0,90,180,270}
+        },{
+            -- center outer edge
+            positions = {{x=2,y=1,z=0}},
+            rules = {
+                ["-1,0,0"] = g_hull,
+                ["0,0,1"] = g_hull,
+                ["0,-1,0"] = g_hull,
+                ["0,1,0"] = g_hull
+            },
+            rotations = {0,90,180,270}
+        },{
+            -- upper corner
+            positions = {{x=0,y=2,z=0}},
+            rules = {
+                ["0,-1,1"] = g_inside,
+                ["-1,-1,0"] = g_inside,
+                ["0,0,-1"] = g_hull,
+                ["1,0,0"] = g_hull,
+                ["0,-1,0"] = g_hull,
+            },
+            rotations = {0,90,180,270}
+        },{
+            -- upper straight
+            positions = {{x=1,y=2,z=0}},
+            rules = {
+                ["0,-1,1"] = g_inside,
+                ["0,0,1"] = g_hull,
+                ["0,-1,0"] = g_hull,
+            },
+            rotations = {0,90,180,270}
+        },{
+            -- upper outer edge
+            positions = {{x=2,y=2,z=0}},
+            rules = {
+                ["-1,-1,1"] = g_inside,
+                ["-1,0,0"] = g_hull,
+                ["0,0,1"] = g_hull,
+                ["0,-1,0"] = g_hull,
+            },
+            rotations = {0,90,180,270}
+        },{
+            -- lower corner
+            positions = {{x=0,y=0,z=0}},
+            rules = {
+                ["0,1,1"] = g_inside,
+                ["-1,1,0"] = g_inside,
+                ["0,0,-1"] = g_hull,
+                ["1,0,0"] = g_hull,
+                ["0,1,0"] = g_hull,
+            },
+            rotations = {0,90,180,270}
+        },{
+            -- lower straight
+            positions = {{x=1,y=0,z=0}},
+            rules = {
+                ["0,1,1"] = g_inside,
+                ["-1,0,0"] = g_hull,
+                ["1,0,0"] = g_hull,
+                ["0,1,0"] = g_hull,
+            },
+            rotations = {0,90,180,270}
+        },{
+            -- lower outer edge
+            positions = {{x=2,y=0,z=0}},
+            rules = {
+                ["-1,1,1"] = g_inside,
+                ["-1,0,0"] = g_hull,
+                ["0,0,1"] = g_hull,
+                ["0,1,0"] = g_hull,
+            },
+            rotations = {0,90,180,270}
+        },{
+            -- top
+            positions = {{x=3,y=2,z=0}},
+            rules = {
+                ["0,1,0"] = g_inside
+            },
             fallback = true,
+            rotations = {0}
+        },{
+            -- bottom
+            positions = {{x=3,y=0,z=0}},
+            rules = {
+                ["0,-1,0"] = g_inside
+            },
             rotations = {0}
         }
     }
 })
 
-mapblock_tileset.register_tileset("hull_slope", {
-    catalog = MP .. "/schematics/hull.zip",
+mapblock_tileset.register_tileset("hull_inside", {
+    catalog = MP .. "/schematics/hull_inside.zip",
     groups = {
-        hull = true,
-        hull_slope = true
-    },
-    disable_orientation = {
-        ["scifi_nodes:blackdmg"] = true
+        hull_inside = true
     },
     tiles = {
         {
-            -- upper slope
-            positions = {{x=1,y=2,z=0}},
-            rules = {
-                ["0,0,1"] = hull_full_rule,
-                ["0,-1,0"] = hull_full_rule
-            },
-            rotations = {0,90,180,270}
-        },{
-            -- lower slope
-            positions = {{x=1,y=0,z=0}},
-            rules = {
-                ["0,0,1"] = hull_full_rule,
-                ["0,1,0"] = hull_full_rule
-            },
-            rotations = {0,90,180,270}
-        },{
-            -- side slope
-            positions = {{x=0,y=1,z=0}},
-            rules = {
-                ["0,0,1"] = hull_full_rule,
-                ["1,0,0"] = hull_full_rule
-            },
-            rotations = {0,90,180,270}
-        },{
-            -- upper edge
-            positions = {{x=0,y=2,z=0}},
-            rules = {
-                ["0,-1,0"] = hull_slope_rule,
-                ["0,0,1"] = hull_slope_rule,
-                ["1,0,0"] = hull_slope_rule
-            },
-            rotations = {0,90,180,270}
-        },{
-            -- lower edge
             positions = {{x=0,y=0,z=0}},
-            rules = {
-                ["0,1,0"] = hull_slope_rule,
-                ["0,0,1"] = hull_slope_rule,
-                ["1,0,0"] = hull_slope_rule
-            },
-            rotations = {0,90,180,270}
+            rules = {},
+            rotations = {0},
+            fallback = true
         }
     }
 })
